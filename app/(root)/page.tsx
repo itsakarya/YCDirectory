82beca7e47473989d560879fe6d9ceeb063e4090
itsakarya <aniket2018pr@gmail.com>
2025-01-19 00:53:35 +0530

    All startup, statupcards & sanity setup

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,70 @@
+import StartupCard from "@/components/StartupCard";
 import SearchForm from "../../components/SearchForm";
 
 export default async function Home({
-    searchParams,
-  }: {
-    searchParams: Promise<{ query?: string }>;
-  }) {
-    const query = (await searchParams).query;
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
+
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 35,
+      author: { id: 1, name: "Aniket" },
+      _id: 1,
+      description: "This is a description",
+      image:
+        "https://codeskulptor-demos.commondatastorage.googleapis.com/GalaxyInvaders/alien_2_1.png",
+      category: "Robots",
+      title: "We Robots",
+    },
+    {
+      _createdAt: new Date(),
+      views: 35,
+      author: { id: 1 },
+      _id: 1,
+      description: "This is a description",
+      image:
+        "https://codeskulptor-demos.commondatastorage.googleapis.com/GalaxyInvaders/alien_2_1.png",
+      category: "Robots",
+      title: "We Robots",
+    },
+    {
+      _createdAt: new Date(),
+      views: 35,
+      author: { id: 1 },
+      _id: 1,
+      description: "This is a description",
+      image:
+        "https://codeskulptor-demos.commondatastorage.googleapis.com/GalaxyInvaders/alien_2_1.png",
+      category: "Robots",
+      title: "We Robots",
+    },
+    {
+      _createdAt: new Date(),
+      views: 35,
+      author: { id: 1 },
+      _id: 1,
+      description: "This is a description",
+      image:
+        "https://codeskulptor-demos.commondatastorage.googleapis.com/GalaxyInvaders/alien_2_1.png",
+      category: "Robots",
+      title: "We Robots",
+    },
+    {
+      _createdAt: new Date(),
+      views: 35,
+      author: { id: 1 },
+      _id: 1,
+      description: "This is a description",
+      image:
+        "https://codeskulptor-demos.commondatastorage.googleapis.com/GalaxyInvaders/alien_2_1.png",
+      category: "Robots",
+      title: "We Robots",
+    },
+  ];
   return (
     <>
       <section className="pink_container">
@@ -18,9 +77,23 @@ export default async function Home({
           Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
           Competitions.
         </p>
-        <SearchForm query={query}/>
+        <SearchForm query={query} />
       </section>
 
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for ${query}` : "All Startups"}
+        </p>
+        <ul className="mt-7 card_grid">
+          {posts.length > 0 ? (
+            posts.map((post, index) => (
+              <StartupCard key={`post-${index}`} post={post} />
+            ))
+          ) : (
+            <p className="no-results"> No startup found</p>
+          )}
+        </ul>
+      </section>
     </>
   );
 }
